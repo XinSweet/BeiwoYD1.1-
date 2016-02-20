c2dc5ad4da7f1eedf7e9815da62986e784b3ed98
skilore <1044993445@qq.com>
2016-02-20 09:42:41 +0800

    Initial Commit

---
+++ BeiwoYD1.1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeiwoYD1.1
+//
+//  Created by 张祚新 on 16/2/20.
+//  Copyright © 2016年 张祚新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeiwoYD1.1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeiwoYD1.1
+//
+//  Created by 张祚新 on 16/2/20.
+//  Copyright © 2016年 张祚新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
